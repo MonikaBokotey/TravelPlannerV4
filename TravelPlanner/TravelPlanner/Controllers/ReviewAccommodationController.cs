9a01001547a557f87281757baafe5712aaeb9514
Monika46 <bokoteymonika@hotmail.com>
2023-05-13 11:07:40 +0300

    no message

---
+++ TravelPlanner/TravelPlanner/Controllers/ReviewAccommodationController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelPlanner.Models;
+
+namespace TravelPlanner.Controllers
+{
+    public class ReviewAccommodationController : Controller
+    {
+        string connectionString = "Data Source=DESKTOP-6A1HP7T;Initial Catalog=TravelPlanner;Integrated Security=True";
+        // GET: ReviewAccommodation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult CreateReview(ReviewAccommodation review, int accommodationId)
+        {
+            LeaveReview(review, accommodationId);
+
+            return Content("Added review");
+        }
+
+
+
+        public void LeaveReview(ReviewAccommodation review,int accomodationId)
+        {
+            int userId = (int)Session["UserId"];
+          
+
+            if (ModelState.IsValid)
+            {
+                review.CreatedAt = DateTime.Now;
+                try
+                {
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        string queryToInsertReview = "INSERT INTO ReviewsAccommodations(UserId, AccommodationId, Rating, Comment, CreatedAt) VALUES (@UserId, @AccommodationId, @Rating, @Comment, @CreatedAt)";
+                        using (SqlCommand command = new SqlCommand(queryToInsertReview, conn))
+                        {
+                            command.Parameters.AddWithValue("@UserId", userId);
+                            command.Parameters.AddWithValue("@AccommodationId", accomodationId);
+                            command.Parameters.AddWithValue("@Rating", review.Rating);
+                           command.Parameters.AddWithValue("@Comment", review.Comment);
+                        //  command.Parameters.AddWithValue("@Comment", ".....");
+                            command.Parameters.AddWithValue("@CreatedAt", review.CreatedAt);
+
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                   
+                }
+                catch (Exception e)
+                {
+                    
+                }
+            }
+           
+        }
+
+
+       
+       /*
+        [HttpPost]
+
+        public ActionResult LeaveReview(ReviewAccommodation review, int accommodationId)
+        {
+            
+          
+            if (ModelState.IsValid)
+            {
+
+                review.CreatedAt = DateTime.Now;
+                try
+                {
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        string queryToInsertReview = "INSERT INTO ReviewsAccommodations(UserId, AccommodationId, Rating, Comment, CreatedAt) VALUES (@UserId, @AccommodationId, @Rating, @Comment, @CreatedAt)";
+                        using (SqlCommand command = new SqlCommand(queryToInsertReview, conn))
+                        {
+                            int userId = (int)Session["UserId"];
+                            command.Parameters.AddWithValue("@UserId", userId);
+                            command.Parameters.AddWithValue("@AccommodationId", accommodationId);
+                            command.Parameters.AddWithValue("@Rating", review.Rating);
+                            // command.Parameters.AddWithValue("@Comment", review.Comment);
+                            command.Parameters.AddWithValue("@Comment", ".....");
+                            command.Parameters.AddWithValue("@CreatedAt", review.CreatedAt);
+
+                            command.ExecuteNonQuery();
+                        }
+                    }
+
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+          
+            return Content("Added review"); ;
+
+
+           
+        }
+       */
+
+        [HttpPost]
+        public ActionResult DisplayReviewAccommodation(int accommodationId)
+        {
+            List<ReviewAccommodation> reviewAccommodations = new List<ReviewAccommodation>();
+
+            string name = Session["firstname"] as string;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string queryToDisplayReview = "SELECT * FROM ReviewsAccommodations WHERE AccommodationId = @AccommodationId";
+                    using (SqlCommand command = new SqlCommand(queryToDisplayReview, conn))
+                    {
+                        command.Parameters.AddWithValue("@AccommodationId", accommodationId);
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                ReviewAccommodation review = new ReviewAccommodation
+                                {
+                                    Rating = (int)reader["Rating"],
+                                    Comment = reader["Comment"].ToString(),
+                                    CreatedAt = (DateTime)reader["CreatedAt"]
+                                };
+                                reviewAccommodations.Add(review);
+                            }
+                        }
+                    }
+                }
+                return View(reviewAccommodations);
+            }
+            catch (Exception e)
+            {
+                return View("Error" + e);
+            }
+        }
+
+
+     
+    }
+}
---
+++ TravelPlanner/TravelPlanner/Controllers/UserController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Mvc;
+using TravelPlanner.Models;
+
+namespace TravelPlanner.Controllers
+{
+    public class UserController : Controller
+    {
+       
+        string connectionString = "Data Source=DESKTOP-6A1HP7T;Initial Catalog=TravelPlanner;Integrated Security=True";
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult New()
+        {
+            return View();
+        }
+        public ActionResult SignedUp()
+        {
+            return View("SignedUp");
+        }
+        public ActionResult SignIn()
+        {
+            return View("SignIn");
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(User user)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string insertQuery = "INSERT INTO Users(LastName, FirstName, Email, UserPassword, UserType) VALUES (@LastName, @FirstName, @Email, @Password, @UserType)";
+
+                using (SqlCommand command = new SqlCommand(insertQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@LastName", user.LastName);
+                    command.Parameters.AddWithValue("@FirstName", user.FirstName);
+                    command.Parameters.AddWithValue("@Email", user.Email);
+                    command.Parameters.AddWithValue("@Password", user.Password);
+
+                    string userType = Request.Form["user-type"];
+                    if (string.IsNullOrEmpty(user.UserType))
+                    {
+                        command.Parameters.AddWithValue("@UserType", "Customer");
+                    }
+                    else
+                    {
+                        command.Parameters.AddWithValue("@UserType", user.UserType);
+                    }
+
+
+                    command.ExecuteNonQuery();
+                }
+            }
+            return RedirectToAction("SignedUp");
+        }
+
+
+        [HttpPost]
+        public ActionResult SignIn(string email, string password)
+        {
+            User user = ValidateUser(email, password);
+
+            if (user == null)
+            {
+                return Content("Wrong email or password! Please try again.");
+            }
+            else
+            {
+                // Add UserId to session
+                Session["UserId"] = user.UserId;
+
+                if (user.UserType == "Customer")
+                {
+                    return RedirectToAction("AccommodationView", "Accommodation");
+                }
+                else if (user.UserType == "Manager")
+                {
+                    return RedirectToAction("NewAccomodation", "Accommodation");
+                }
+                else if (user.UserType == "Local")
+                {
+                    return RedirectToAction("NewActivity", "Activity");
+                }
+                else
+                {
+                    return RedirectToAction("AccommodationView", "Accommodation");
+                }
+            }
+        }
+
+
+        private User ValidateUser(string email, string password)
+        {
+            User user = null;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string selectQuery = "SELECT * FROM Users WHERE Email = @Email AND UserPassword = @Password";
+
+                using (SqlCommand command = new SqlCommand(selectQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@Email", email);
+                    command.Parameters.AddWithValue("@Password", password);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            user = new User
+                            {
+                                UserId = (int)reader["UserId"],
+                                LastName = (string)reader["LastName"],
+                                FirstName = (string)reader["FirstName"],
+                                Email = (string)reader["Email"],
+                                Password = (string)reader["UserPassword"],
+                                UserType = (string)reader["UserType"],
+                            };
+                          
+                        }
+                    }
+                }
+            }
+
+
+            return user;
+        }
+
+
+    }
+}
